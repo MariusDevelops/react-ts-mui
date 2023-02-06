971c339785663425a32cd2a9c107210a59ea577c
MariusDevelops <brazasmarius@gmail.com>
2023-02-06 20:06:39 +0200

    feat: typography-page

--- src/pages/typography-page.tsx
+++ src/pages/typography-page.tsx
@@ -1,7 +1,25 @@
+/* eslint-disable max-len */
 import React from 'react';
+import { Typography } from '@mui/material';
 
 const TypographyPage = () => (
-  <div>TypographyPage</div>
+  <div>
+    <Typography variant="h1">h1 Heading</Typography>
+    <Typography variant="h2">h2 Heading</Typography>
+    <Typography variant="h3">h3 Heading</Typography>
+    <Typography variant="h4" component="h1" gutterBottom>h4 Heading</Typography>
+    <Typography variant="h5">h5 Heading</Typography>
+    <Typography variant="h6">h6 Heading</Typography>
+
+    <Typography variant="subtitle1">Sub title 1</Typography>
+    <Typography variant="subtitle2">Sub title 2</Typography>
+
+    <Typography variant="body1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi rerum doloremque officia saepe impedit dolor, eum mollitia magnam, enim explicabo quo similique, non porro perferendis obcaecati temporibus recusandae cupiditate facere!</Typography>
+    <Typography>
+      Default value. Same as body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi rerum doloremque officia saepe impedit dolor, eum mollitia magnam, enim explicabo quo similique, non porro perferendis obcaecati temporibus recusandae cupiditate facere!
+    </Typography>
+    <Typography variant="body2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod soluta deserunt cum fuga ullam placeat. Saepe temporibus quis eius, repellendus officiis suscipit, repudiandae nobis et optio quibusdam, ratione doloribus. Nemo.</Typography>
+  </div>
 );
 
 export default TypographyPage;
