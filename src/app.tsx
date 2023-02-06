608f8179aa3cd4651bb456d43d78a14f34bdbf38
MariusDevelops <brazasmarius@gmail.com>
2023-02-06 21:19:44 +0200

    feat: button-page

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import TypographyPage from 'pages/typography-page';
+import ButtonPage from 'pages/button-page';
 
 const App = () => (
-  <TypographyPage />
+  <>
+    <TypographyPage />
+    <ButtonPage />
+  </>
+
 );
 
 export default App;
