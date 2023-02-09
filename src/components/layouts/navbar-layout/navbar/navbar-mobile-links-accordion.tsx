c08792d2acfc5ee11428689054148c2164e453f7
MariusDevelops <brazasmarius@gmail.com>
2023-02-09 22:13:53 +0200

    feat: open/close accordion tabs

--- src/components/layouts/navbar-layout/navbar/navbar-mobile-links-accordion.tsx
+++ src/components/layouts/navbar-layout/navbar/navbar-mobile-links-accordion.tsx
@@ -6,42 +6,43 @@ import {
   AccordionSummary,
   Typography,
   AccordionDetails,
+  type AccordionProps,
 } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { useLocation } from 'react-router-dom';
 import NavbarMobileLink from './navbar-mobile-link';
 import LinkData from './link-data';
 
 type NavbarMobileLinksAccordionProps = {
   title: string,
   linksData: LinkData[],
-  closeMenu: VoidFunction
+  closeMenu: VoidFunction,
+  expanded: boolean,
+  onChange: AccordionProps['onChange'],
+  hasActiveLink: boolean
 };
 
 const NavbarMobileLinksAccordion: React.FC<NavbarMobileLinksAccordionProps> = ({
   title,
   linksData,
   closeMenu,
-}) => {
-  const { pathname } = useLocation();
-  const hasActiveLink = linksData.map<string>(({ link }) => link).includes(pathname);
-
-  return (
-    <Accordion defaultExpanded={hasActiveLink} disableGutters>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <Typography color={hasActiveLink ? 'primary' : 'inherit'}>{title}</Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <MenuList sx={{ p: 0 }}>
-          {linksData.map(({ link, text }) => (
-            <MenuItem key={link} onClick={closeMenu} sx={{ p: 0 }}>
-              <NavbarMobileLink to={link}>{text}</NavbarMobileLink>
-            </MenuItem>
-          ))}
-        </MenuList>
-      </AccordionDetails>
-    </Accordion>
-  );
-};
+  expanded,
+  onChange,
+  hasActiveLink,
+}) => (
+  <Accordion expanded={expanded} onChange={onChange} disableGutters>
+    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+      <Typography color={hasActiveLink ? 'primary' : 'inherit'}>{title}</Typography>
+    </AccordionSummary>
+    <AccordionDetails>
+      <MenuList sx={{ p: 0 }}>
+        {linksData.map(({ link, text }) => (
+          <MenuItem key={link} onClick={closeMenu} sx={{ p: 0 }}>
+            <NavbarMobileLink to={link}>{text}</NavbarMobileLink>
+          </MenuItem>
+        ))}
+      </MenuList>
+    </AccordionDetails>
+  </Accordion>
+);
 
 export default NavbarMobileLinksAccordion;
--- src/components/layouts/navbar-layout/navbar/navbar-mobile-menu.tsx
+++ src/components/layouts/navbar-layout/navbar/navbar-mobile-menu.tsx
@@ -12,6 +12,7 @@ import {
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import { useLocation } from 'react-router-dom';
 import NavbarMobileLink from './navbar-mobile-link';
 import LinkData from './link-data';
 import NavbarMobileLinksAccordion from './navbar-mobile-links-accordion';
@@ -30,25 +31,49 @@ const NavbarMobileMenu: React.FC<NavbarMobileMenuProps> = ({
   linksData,
   linksGroups,
 }) => {
-  const [drawerOpen, setDrawerOpen] = React.useState<boolean>(false);
-  const isExpanded = useMediaQuery((theme: Theme) => theme.breakpoints.up(expandBreakpoint));
+  const { pathname } = useLocation();
+  let activeGroupTitle: string | false = false;
+  for (let i = 0; i < linksGroups.length; i += 1) {
+    const linksGroup = linksGroups[i];
+    const hasActiveLink = linksGroup.linksData
+      .map<string>(({ link }) => link)
+      .includes(pathname);
 
-  const closeMenu = () => setDrawerOpen(false);
+    if (hasActiveLink) {
+      activeGroupTitle = linksGroup.title;
+      break;
+    }
+  }
+
+  const [menuOpen, setMenuOpen] = React.useState<boolean>(false);
+  const isExpanded = useMediaQuery((theme: Theme) => theme.breakpoints.up(expandBreakpoint));
+  const [
+    openedAccordionTitle,
+    setOpenedAccordionTitle,
+  ] = React.useState<string | false>(activeGroupTitle);
+
+  const closeMenu = () => setMenuOpen(false);
+  const handleAccordionAction = (groupTitle: string) => (
+    event: React.SyntheticEvent,
+    accordionOpen: boolean,
+  ) => {
+    setOpenedAccordionTitle(accordionOpen ? groupTitle : false);
+  };
 
   return (
     <>
       <IconButton
         sx={{ display: { xs: 'flex', [expandBreakpoint]: 'none' } }}
-        onClick={() => setDrawerOpen(!drawerOpen)}
+        onClick={() => setMenuOpen(!menuOpen)}
       >
-        {drawerOpen
+        {menuOpen
           ? <CloseIcon sx={{ color: 'common.white', fontSize: 25 }} />
           : <MenuIcon sx={{ color: 'common.white', fontSize: 25 }} />}
       </IconButton>
       <Drawer
         anchor="top"
-        open={drawerOpen && !isExpanded}
-        onClose={() => setDrawerOpen(false)}
+        open={menuOpen && !isExpanded}
+        onClose={() => setMenuOpen(false)}
       >
         <Box>
           <Toolbar />
@@ -64,6 +89,9 @@ const NavbarMobileMenu: React.FC<NavbarMobileMenuProps> = ({
                 title={linksGroup.title}
                 linksData={linksGroup.linksData}
                 closeMenu={closeMenu}
+                expanded={openedAccordionTitle === linksGroup.title}
+                onChange={handleAccordionAction(linksGroup.title)}
+                hasActiveLink={activeGroupTitle === linksGroup.title}
               />
             ))}
           </MenuList>
